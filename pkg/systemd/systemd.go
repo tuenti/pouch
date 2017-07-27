51289c3a4de83d29b29e1035b2b227b8a3237c8d
Jaime Soriano Pastor <jsoriano@tuenti.com>
2017-07-27 12:49:55 +0200

    Don't try to read unit name to decide if systemd is available
    In previous versions of pouch we used unit name, but
    we are not using it anymore, this prevents pouch of using
    systemd integrations if it's not run from a unit or if it
    cannot get the unit name if its containerized.

--- pkg/systemd/systemd.go
+++ pkg/systemd/systemd.go
@@ -21,7 +21,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strings"
 
 	"github.com/coreos/go-systemd/daemon"
 	"github.com/coreos/go-systemd/dbus"
@@ -31,7 +30,6 @@ import (
 type SystemD interface {
 	IsAvailable() bool
 	CanNotify() bool
-	UnitName() (string, error)
 	Close()
 
 	NotifyReady() error
@@ -57,22 +55,6 @@ type systemd struct {
 func (s *systemd) Close() {
 }
 
-func (s *systemd) getName() (string, error) {
-	if s.name != "" {
-		return s.name, nil
-	}
-	name, err := util.CurrentUnitName()
-	if err != nil {
-		return "", err
-	}
-	s.name = name
-	return s.name, nil
-}
-
-func (s *systemd) UnitName() (string, error) {
-	return s.getName()
-}
-
 func (s *systemd) IsAvailable() bool {
 	if !s.enabled {
 		return false
@@ -83,17 +65,7 @@ func (s *systemd) IsAvailable() bool {
 		return false
 	}
 
-	name, err := s.getName()
-	if err != nil {
-		log.Printf("Couldn't obtain current unit name: %v", err)
-		return false
-	}
-	if !strings.HasSuffix(name, ".service") {
-		log.Printf("Process is not started from a service unit, unit name found: %s", name)
-		return false
-	}
-
-	log.Printf("systemd available, unit name: %s\n", name)
+	log.Println("systemd seems to be available")
 
 	return true
 }
